aa24f73461b77ec10c374b145e93a6f6b7e9dd02
sahiljain11 <sahilbot1@gmail.com>
2020-01-06 14:10:44 -0600

    Added registers

--- app/main.rb
+++ app/main.rb
@@ -9,7 +9,31 @@ class Mips
   end
 
   def defaults
+    state.num_registers ||= 64   #32 32-bit integer and logic instructions and 32-bit floating point instructions
+    state.registers     ||= Array.new(state.num_registers)
+    state.register_hash ||= Hash.new
 
+    create_register_hash() if state.register_hash.length == 0
+  end
+
+  def create_register_hash
+    #create hash of all registers
+    register_names = ["zero", "at", "v", "a", "t", "s", "t", "k", "gp", "sp", "fp", "ra", "f"]
+    register_count = [     1,    1,   2,   4,   8,   8,   2,   2,    1,    1,    1,    1,  32]
+    count = 0
+    num_t = 0   #keeps track of number of t registers
+
+    register_count.length.times do |n|
+      register_count[n].times do |m|
+        if register_names[n] != "t"
+          state.register_hash["$" + register_names[n] + m.to_s] = count
+        else
+          state.register_hash["$t" + num_t.to_s] = count
+          num_t += 1
+        end
+        count += 1
+      end
+    end
   end
 
   def render
