5d988ed6c28c8f7e904f65e06b33593d9071850f
sahiljain11 <sahilbot1@gmail.com>
2020-01-06 16:00:51 -0600

    Added basic GUI

--- app/main.rb
+++ app/main.rb
@@ -9,24 +9,26 @@ class Mips
   end
 
   def defaults
-    state.num_registers ||= 64   #32 32-bit integer and logic instructions and 32-bit floating point instructions
-    state.registers     ||= Array.new(state.num_registers)
-    state.register_hash ||= Hash.new
+    state.num_registers  ||= 64   #32 integer and logic instructions and 32 floating point instructions
+    state.registers      ||= Array.new(state.num_registers)
+    state.register_hash  ||= Hash.new
+    state.register_names ||= ["zero", "at", "v", "a", "t", "s", "t", "k", "gp", "sp", "fp", "ra", "f"]
+    state.register_count ||= [     1,    1,   2,   4,   8,   8,   2,   2,    1,    1,    1,    1,  32]
 
     create_register_hash() if state.register_hash.length == 0
+
+    #state.render_state = 
   end
 
   def create_register_hash
     #create hash of all registers
-    register_names = ["zero", "at", "v", "a", "t", "s", "t", "k", "gp", "sp", "fp", "ra", "f"]
-    register_count = [     1,    1,   2,   4,   8,   8,   2,   2,    1,    1,    1,    1,  32]
     count = 0
     num_t = 0   #keeps track of number of t registers
 
-    register_count.length.times do |n|
-      register_count[n].times do |m|
-        if register_names[n] != "t"
-          state.register_hash["$" + register_names[n] + m.to_s] = count
+    state.register_count.length.times do |n|
+      state.register_count[n].times do |m|
+        if state.register_names[n] != "t"
+          state.register_hash["$" + state.register_names[n] + m.to_s] = count
         else
           state.register_hash["$t" + num_t.to_s] = count
           num_t += 1
@@ -37,7 +39,27 @@ class Mips
   end
 
   def render
+    outputs.borders << [ 50,  50, 500, 570]
+    outputs.borders << [720,  50, 500, 250]
+    outputs.borders << [720, 370, 500, 250]
 
+    outputs.labels << [300, 667, "Registers", 5, 1]
+    outputs.labels << [970, 667, "File", 5, 1]
+    outputs.labels << [970, 345, "Console", 5, 1]
+
+    tempX = 67
+    tempY = 607
+    count = 0
+    state.register_count.length.times do |n|
+      state.register_count[n].times do |m|
+        outputs.labels << [tempX, tempY, "$" + state.register_names[n] + m.to_s + ": " +
+                                         state.registers[count].to_s, 1]
+        count += 1
+        tempY -= 35
+        tempX += 125 if count % 16 == 0
+        tempY =  607 if count % 16 == 0
+      end
+    end
   end
 
   def calc
