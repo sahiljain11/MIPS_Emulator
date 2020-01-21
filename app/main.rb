f24d177a5e80a73e5956b378082d8e35abebb8a6
Sahil Jain <sahilbot1@gmail.com>
2020-01-21 00:47:36 -0600

    Working on function processing. Kinda just broke everything oops

--- app/main.rb
+++ app/main.rb
@@ -12,10 +12,15 @@ class Mips
     state.num_registers  ||= 64   #32 integer and logic instructions and 32 floating point instructions
     state.registers      ||= Array.new(state.num_registers)
     state.register_hash  ||= Hash.new
+    state.function_hash  ||= Hash.new
     state.register_names ||= ["zero", "at", "v", "a", "t", "s", "t", "k", "gp", "sp", "fp", "ra", "f"]
     state.register_count ||= [     1,    1,   2,   4,   8,   8,   2,   2,    1,    1,    1,    1,  32]
+    state.current_line   ||= -1
+    state.input          ||= gtk.read_file("app/input.txt").split("\r\n")
 
     create_register_hash() if state.register_hash.length == 0
+    create_function_hash() if state.function_hash.length == 0
+    process_function() if state.current_line == -1
   end
 
   def create_register_hash
@@ -36,7 +41,13 @@ class Mips
     end
   end
 
+  def create_function_hash
+    state.function_hash["li"]   = 0
+    state.function_hash["move"] = 0
+  end
+
   def render
+    #Render basic graphics
     outputs.borders << [ 50,  50, 500, 570]
     outputs.borders << [720,  50, 500, 250]
     outputs.borders << [720, 370, 500, 250]
@@ -45,6 +56,7 @@ class Mips
     outputs.labels << [970, 667, "File", 5, 1]
     outputs.labels << [970, 345, "Console", 5, 1]
 
+    #Render all the registers
     tempX = 67
     tempY = 607
     count = 0
@@ -58,14 +70,51 @@ class Mips
         tempY =  607 if count % 16 == 0
       end
     end
+
+    #Render the File Text
+    start = state.current_line - 6
+    tempX = 730
+    tempY = 613
+    increasingAlpha = 30
+    7.times do |n|
+      if start + n < state.input.length && start + n >= 0
+        #puts(state.input[start + n] + " " + (start + n).to_s)
+        outputs.labels << [tempX, tempY, state.input[start + n] + "", 1, 0, 0, 0, 0, increasingAlpha]
+      end
+      tempY -= 35
+      increasingAlpha += 31
+    end
   end
 
   def calc
-    input = gtk.read_file("input.txt")
+
   end
 
   def process_inputs
+    if inputs.keyboard.space && state.current_line + 1 != state.inputs.length
+      process_function()
+    end
+  end
 
+  def process_function
+      state.curent_line += 1
+      current_line = state.inputs[state.current_line].split(" ")
+
+      if state.function_hash.has_key?(current_line[0])
+        #Execute said function
+        determine_function(current_line[0], current_line[1].chop, current_line[2]) #chop removes the comma
+      else
+        #It's a variable
+      end
+  end
+
+  def determine_function (func, param1, param2)
+    li(param1, param2) if func == "li"
+  end
+
+  def li (var, integer)
+    index = state.register_hash.fetch(param1)
+    state.registers[index] = integer
   end
   
 end
@@ -73,10 +122,11 @@ end
 $mips = Mips.new
 
 def tick args
-    $mips.grid    = args.grid
-    $mips.inputs  = args.inputs
-    $mips.state   = args.state
-    $mips.outputs = args.outputs
-    $mips.gtk     = args.gtk
-    $mips.tick
+  args.gtk.log_level = :off
+  $mips.grid    = args.grid
+  $mips.inputs  = args.inputs
+  $mips.state   = args.state
+  $mips.outputs = args.outputs
+  $mips.gtk     = args.gtk
+  $mips.tick
 end
